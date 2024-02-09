cd9664d1465e24811a75e324abe8aba1dc5918db
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 10:28:20 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- Generics/PersonModel.cs
+++ Generics/PersonModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Generics
+﻿namespace Generics
 {
-	class PersonModel
+	internal class PersonModel
 	{
 		public string FirstName
 		{
