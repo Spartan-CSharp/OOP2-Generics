c86f7c6c9a5f8b1f6cb73edb9d3a5bd6155ac41a
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 10:27:22 -0500

    Create Code
    Create a generic method that takes in an item and calls the ToString() method and prints that value to the Console.

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,6 +10,31 @@ namespace Generics
 	{
 		static void Main(string[] args)
 		{
+			string teststring = "A String";
+			int testint = 42;
+			bool testbool = false;
+			double testdouble = 98.6;
+			PersonModel testpersonmodel = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+
+			string stringstringified = Stringify(teststring);
+			string intstringified = Stringify(testint);
+			string boolstringified = Stringify(testbool);
+			string doublestringified = Stringify(testdouble);
+			string personmodelstringified = Stringify(testpersonmodel);
+
+			Console.WriteLine($"{teststring} stringified is {stringstringified}");
+			Console.WriteLine($"{testint} stringified is {intstringified}");
+			Console.WriteLine($"{testbool} stringified is {boolstringified}");
+			Console.WriteLine($"{testdouble} stringified is {doublestringified}");
+			Console.WriteLine($"{testpersonmodel} stringified is {personmodelstringified}");
+
+			_ = Console.ReadLine();
+		}
+
+		private static string Stringify<T>(T item)
+		{
+			string output = item.ToString();
+			return output;
 		}
 	}
 }
